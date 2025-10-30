a2fce1eb986460b520f9c574cb4fb607e5767b7a
CupChailatte <Chaisoda864@gmail.com>
2025-10-30 18:31:46 +0100

    Add project files.

---
+++ C#_Fundamentals/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+namespace System;  
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        Person person1 = new Person("Alice", 34);
+        person1.SayHello(); 
+        
+    }
+
+
+}
+
+
+
+public class Person
+{
+    public string Name { get; set; }
+    public int Age {  get; set; }
+
+    public Person(string name, int age) //constructor 
+    {
+        Name = name;
+        Age = age; 
+    }
+
+    public void SayHello()
+    {
+        Console.WriteLine("Hello my name is " + Name);
+    }
+}
